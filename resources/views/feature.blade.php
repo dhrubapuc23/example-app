04c3070384eefed7fe999c2c515a8a1905c4ee82
Dhruba <dhrubajyoti1212@gmail.com>
2025-08-22 00:03:45 +0600

    practice files added

---
+++ resources/views/feature.blade.php
@@ -0,0 +1,48 @@
+@extends('dashboard')
+
+@section('info')
+    <div class="col-md-12">
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+    </div>
+@endsection
+
+@section('content')
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\StudentController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [UserController::class, 'index']);
+// Route::get('/homepage', function () {
+//     return view('homepage');
+// });
+
+Route::view('/homepage', 'homepage', ['title' => 'Homepage'])->name('hp');
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+// Route::redirect('/', '/homepage');
+Route::get('/user/{id}/{name?}', function (string $id,string $name = null) {
+    // return 'User '.$id.'Name '.$name;
+    return view('userpage',['id' => $id, 'name' => $name]);
+})->whereNumber('id')->whereAlpha('name');
+
+Route::prefix('blog')->group(function () {
+    Route::get('/view', function () {
+        return 'This is the blog view page';
+    });
+    Route::get('/create', function () {
+        return 'This is the blog create page';
+    });
+    Route::get('/update', function () {
+        return 'This is the blog update page';
+    });
+    Route::get('/delete', function () {
+        return 'This is the blog delete page';
+    });
+});
+
+Route::fallback(function () {
+    return 'This is a fallback route';
+});
+
+// Route::redirect('old url', 'new url');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/feature', function () {
+    return view('feature');
+});
+
+Route::get('/student', [StudentController::class, 'index']);
+//Route::resource('photo', PhotoController::class);
+Route::get('/getcourse', [StudentController::class, 'getcoursedetails']);
